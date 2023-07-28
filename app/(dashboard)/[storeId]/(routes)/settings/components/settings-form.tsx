7e76225c950fee370f97057f025dcccc7add7457
aghyadghziel <EDGXaghyad@gmail.com>
2023-07-28 14:17:30 +0300

    g

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -8,6 +8,15 @@ import { Trash } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface SettingsFormProps {
   initialData: Store;
@@ -17,6 +26,7 @@ const formSchema = z.object({
 });
 
 type SettingsFormValues = z.infer<typeof formSchema>;
+
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -24,6 +34,10 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
     resolver: zodResolver(formSchema),
     defaultValues: initialData,
   });
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    console.log(data);
+  };
   return (
     <>
       <div className="flex items-center justify-between">
@@ -33,6 +47,40 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
         </Button>
       </div>
       <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Store name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button
+            disabled={loading}
+            onClick={() => setOpen(true)}
+            className="ml-auto"
+            type="submit"
+          >
+            Save Changes
+          </Button>
+        </form>
+      </Form>
     </>
   );
 };
