760e525622a24c82f21e7271becaf6585bc9fa74
Heyfa Doraghi <heyfa@Heyfas-Macbook-Pro.local>
2023-08-16 00:57:09 +0400

    d

--- components/ui/image-uplaod.tsx
+++ components/ui/image-uplaod.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { Button } from "./button";
+import { Trash } from "lucide-react";
 
 interface ImageUploadProps {
   disabled?: boolean;
@@ -29,7 +31,26 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
   }
   return (
     <div>
-      <div className="mb-4 flex items-center gap-4"></div>
+      <div className="mb-4 flex items-center gap-4">
+        {value.map((url) => (
+          <div
+            key={url}
+            className="relative w-[200px] h-[200px] rounded-md overflow-hidden"
+          >
+            {" "}
+            <div className="z-10 absoulute top-2 righ-2">
+              <Button
+                type="button"
+                onClick={() => onRemove(url)}
+                variant="destructive"
+                size="icon"
+              >
+                <Trash className="h-4 w-4" />
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
